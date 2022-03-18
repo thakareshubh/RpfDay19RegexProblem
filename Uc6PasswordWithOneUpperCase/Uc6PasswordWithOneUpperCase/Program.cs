824af455fdf59b8aafd3dffb6ba7bffc4e67b774
thakareshubh <shubhamthakare032@gmail.com>
2022-03-18 16:53:04 +0530

    Added code with uppercase pattern

---
+++ Uc6PasswordWithOneUpperCase/Uc6PasswordWithOneUpperCase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Text.RegularExpressions;
+
+namespace PassWordWithOneUpperCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Enter Password");
+
+            string password = Console.ReadLine();
+
+
+            if (Regex.Match(password, @"^([a-z]{5})([@]{1})([A-Z]{1})([0-9]{1})$").Success)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
