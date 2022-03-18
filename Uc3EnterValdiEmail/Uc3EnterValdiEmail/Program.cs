860c64ef76b3e842133e4f5c70a35588af2c5fe4
thakareshubh <shubhamthakare032@gmail.com>
2022-03-18 09:35:46 +0530

    Added emailcheck Regex problem

---
+++ Uc3EnterValdiEmail/Uc3EnterValdiEmail/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EnterValidEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Enter Email Address");
+
+            string email = Console.ReadLine();
+
+
+            if (Regex.Match(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$").Success)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
