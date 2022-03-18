f0311f17c396985aedda3913550d3092aaa7c874
thakareshubh <shubhamthakare032@gmail.com>
2022-03-18 16:59:39 +0530

    Added clear mail pattern

---
+++ Uc9ClearEmailAddress/Uc9ClearEmailAddress/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Text.RegularExpressions;
+
+namespace RegexPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Enter Email");
+
+            string email = Console.ReadLine();
+
+
+            if (Regex.Match(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$").Success)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
