c1ee4928ebec82b8a1c73b8855ffaf051351b1e2
thakareshubh <shubhamthakare032@gmail.com>
2022-03-18 09:38:05 +0530

    Added mobile number regex problem

---
+++ Uc4PreDefienedMobileFormat/Uc4PreDefienedMobileFormat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PreDefienedMobileFormat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Console.WriteLine("Enter Mobile Number");
+
+            string mobileNo = Console.ReadLine();
+
+
+            if (Regex.Match(mobileNo, @"^([0-9]{2})\s([0-9]{10})$").Success)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
