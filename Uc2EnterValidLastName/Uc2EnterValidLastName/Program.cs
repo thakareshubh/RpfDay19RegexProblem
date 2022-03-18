6bb7ffd9c3db67a888192f64d7122c6434b3c350
thakareshubh <shubhamthakare032@gmail.com>
2022-03-18 09:32:27 +0530

    Added last name problem

---
+++ Uc2EnterValidLastName/Uc2EnterValidLastName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter Last Name");
+
+            string text = Console.ReadLine();
+
+            if (Regex.Match(text, "^[A-Z][a-z]{1,3}$").Success)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
